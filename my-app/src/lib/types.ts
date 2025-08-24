a934cf09f46d76f56ea83ce8a3df5d849a20f348
nathan <49448184+tatsuya98@users.noreply.github.com>
2025-08-24 19:58:19 +0100

    nav and home

---
+++ my-app/src/lib/types.ts
@@ -0,0 +1,6 @@
+export type collection = {
+    id: number,
+    title: string
+}
+
+export const activeState:[string, string] = ["active", "inactive"];
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+
+<div class="flex-container">
+    <div class="hero">
+        <h1>Search</h1>
+        <p>high-resolution images from Unsplash</p>
+    </div>
+    <input type="text" placeholder="Enter your keywords...">
+</div>
+
+
+<style>
+    p{
+        font-size: .875rem;
+    }
+    input{
+        width: 80%;
+        padding: 15px;
+        background-image: url("$lib/assets/Search.svg");
+        background-repeat: no-repeat;
+        background-position: 270px;
+    }
+    .flex-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vw;
+        font-family: 'Be Vietnam+Pro',sans-serif;
+    }
+    .hero{
+        text-align: center;
+        margin-bottom: 10px;
+    }
+    h1{
+        margin: 0;
+    }
+</style>
